411bacf341009627fc1b58e6f1c1c1b42f970ea9
kobrynovych <kobrynovych@gmail.com>
2026-06-27 16:10:48 +0300

    fix console err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html
       lang="uk"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <ThemeScript />
